59b7cfa8cf036b08a65c732864201bd9d80681fb
Aleksander Grumic <aleksander.grumic@utb.ecutbildning.se>
2022-12-17 17:04:51 +0100

    Fixed stupidm print error...

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ subprocess.call('clear', shell=True)
 remoteServer = input("Enter a remote host to scan:")
 remoteServerIP = socket.gethostbyname(remoteServer)
 
-print "_" * 60
-print "Please wait, scanning remote host", remoteServerIP
-print "_" * 60
+print("_") * 60
+print("Please wait, scanning remote host"), remoteServerIP
+print("_") * 60
 
 time1 = datetime.now()
 
@@ -20,23 +20,23 @@ try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     result = sock.connect_ex((remoteServerIP, port))
     if result == 0:
-        print "Port {}:          Open".format(port)
+        print("Port {}:          Open").format(port)
         sock.close()
 
 except KayboardInterrupt:
-    print "You pressed Ctrl+C"
+    print("You pressed Ctrl+C")
     sys.exit()
 
 except socket.gaierror:
-    print "Hostname could not be resolved. Exiting"
+    print("Hostname could not be resolved. Exiting")
     sys.exit()
 
 except socket.error:
-    print "Couldn't connect to server"
+    print("Couldn't connect to server")
     sys.exit()
 
 time2 = datetime.now()
 
 total = time2 - time1
 
-print "Scanning Completed in", total
+print("Scanning Completed in"), total
