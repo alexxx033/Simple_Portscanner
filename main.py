58c288b7de73c392ba7ef6d07ed4ced4b428be4b
Aleksander Grumic <aleksander.grumic@utb.ecutbildning.se>
2022-12-19 11:13:39 +0100

    trying to import nmap instead, alot of err now

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import socket
 # import os
 import sys
+import nmap
 
 from datetime import datetime
+
+nmap_path = r"C:\Program Files (x86)\Nmap\nmap.exe"
+nmap.PortScanner(nmap_search_path=nmap_path)
 
 # os.system('clear')
 print('\033c')
 
-remoteServer = input("Enter a remote host to scan:")
-start_port = int(input("Enter start port: "))
-end_port = int(input("Enter end port: "))
-remoteServerIP = socket.gethostbyname(remoteServer)
+### NMAP starts here ###
+nm = nmap.PortScanner()
+nm.scan = input("Enter a IP to scan:")
+
+for host in nm.all_hosts():
+    print('Host : %s (%s)' % (host, nm[host].hostname()))
+    print('State : %s' % nm[host].state())
+    for proto in nm[host].all_protocols():
+        print('Protocol : %s' % proto)
+
+        lport = nm[host][proto].keys()
+        lport.sort()
+        for port in lport:
+            print('port : %s\tstate : %s' %
+                  (port, nm[host][proto][port]['state']))
+### NMAP stops here ###
+
+#remoteServer = input("Enter a remote host to scan:")
+#start_port = int(input("Enter start port: "))
+#end_port = int(input("Enter end port: "))
+#remoteServerIP = socket.gethostbyname(remoteServer)
 
 print("_"*60)
 print("Please wait, scanning remote host", remoteServerIP)
@@ -19,12 +40,18 @@ print("_"*60)
 time1 = datetime.now()
 
 try:
+
+    total_ports = end_port - start_port + 1
+    count = 0
     for port in range(start_port, end_port + 1):
+        count += 1
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         if result == 0:
             print("Port {}: Open".format(port))
-    sock.close()
+        sock.close()
+        print("Checked {} of {} ports".format(count, total_ports))
+
 
 except socket.error as err:
     print("Socket creation failed with error %s" % (err))
